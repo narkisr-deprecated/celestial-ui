a31cef413c29c49d4248c46bf4abadda9d7fa54b
Ronen Narkis <narkisr@gmail.com>
2013-09-27 03:29:54 +0300

    adding type support

--- src/app/systems/add/add.js
+++ src/app/systems/add/add.js
@@ -13,14 +13,34 @@ angular.module( 'celestial.systemAdd', [
     data:{ pageTitle: 'New System' }
   });
 })
-.controller( 'SystemAddCtrl', function SystemAddController($scope,$resource) {
+.controller( 'SystemAddCtrl', function SystemAddController($scope,$http) {
 
   $scope.system = {};
-  $scope.system.hypervisor = 'Proxmox';
+
+  $scope.system.hypervisor = 'proxmox';
+
+  $scope.loadForm = function () {
+    $http({method: 'GET', url: '/types'}).
+      success(function(data, status, headers, config) {
+        $scope.types = [];
+        angular.forEach(data.types,function(type){
+           $scope.types.push(type);
+        });
+        $scope.system.type = $scope.types[0];
+      }).error(function(data, status, headers, config) {
+        console.log('failed to featc types');
+      });
+  };
 
   $scope.hypervisorSelect = function() {
-    $scope.hypervisorTemplate = 'systems/add/'+$scope.system.hypervisor.toLowerCase()+'.tpl.html';
+    $scope.hypervisorTemplate = 'systems/add/'+$scope.system.hypervisor+'.tpl.html';
   };
 
   $scope.$watch( 'system.hypervisor', $scope.hypervisorSelect );
+
+  $scope.submit = function(){
+    console.log($scope.system);
+  };
+  
+  $scope.loadForm();
 });
