009c66a54798af7d83d3cff16e8f24ea18e5e74c
Ronen Narkis <narkisr@gmail.com>
2013-10-04 17:48:22 +0300

    supporting action launch

--- src/app/systems/systems.js
+++ src/app/systems/systems.js
@@ -1,6 +1,7 @@
-
 angular.module( 'celestial.systems', [
-  'ui.state', 'ui.bootstrap', 'ngResource','celestial.system','celestial.systemAdd','angular-growl', 'ngAnimate'
+  'ui.state', 'ui.bootstrap', 'ngResource',
+  'celestial.system', 'celestial.systemAdd', 'celestial.actions',
+  'angular-growl', 'ngAnimate'
 ])
 .config(function config($stateProvider) {
   $stateProvider.state( 'systems', {
@@ -14,7 +15,7 @@ angular.module( 'celestial.systems', [
     data:{ pageTitle: 'Systems' }
   });
 })
-.controller( 'SystemsCtrl', function SystemsController($scope,$resource,growl) {
+.controller( 'SystemsCtrl', function SystemsController($scope, $resource, $http, actionsService, growl) {
 
   var Systems = $resource('/systems/', {page:'@page',offset:'@offset'});
   var Jobs = $resource('/jobs/', {},{
@@ -24,19 +25,17 @@ angular.module( 'celestial.systems', [
    destroy:{method : "POST", params:{id:'@id'},url:'/jobs/destroy/:id'}
   });
 
-  $scope.perPage = 10;
 
+  $scope.perPage = 10;
   $scope.currentPage = 1;
 
   $scope.loadCount = function(){
      Systems.get({page:1,offset:$scope.perPage},function(data,resp){
         if(data.meta!=null){
-         $scope.count = data.meta.total;
+          $scope.count = data.meta.total;
         }
      });
   };
-
-  $scope.loadCount();
 
   $scope.setPage = function () {
     var page = {page:$scope.currentPage,offset: $scope.perPage};
@@ -47,15 +46,21 @@ angular.module( 'celestial.systems', [
         system[1]['hypervisor'] = _.filter(['aws','proxmox','vsphere'],function(a){
            return system[1][a]!=null;
         })[0];
+        system[1]['actions'] = actionsService.loadActions(system[1].type);
+        
         $scope.systems.push(system[1]);
 	});
     });
   };
   
-  $scope.launchJob = function(id,action) {
-   Jobs[action]({id:id},function(data) {
-      growl.addInfoMessage(data.msg);
-   });
+  $scope.launchJob = function(id,job) {
+    Jobs[job]({id:id},function(data) {
+       growl.addInfoMessage(data.msg);
+    });
+  };
+
+  $scope.launchAction = function(id,action) {
+    actionsService.launchAction(id,action);
   };
 
   $scope.$watch( 'currentPage', $scope.setPage );
