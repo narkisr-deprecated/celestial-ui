9cae392916e6d2c83cc89dcba06a605ab411ccc0
Ronen Narkis <narkisr@gmail.com>
2013-10-04 17:48:05 +0300

    adding actions server

---
+++ src/app/systems/actions/actions.js
@@ -0,0 +1,25 @@
+angular.module('celestial.actions', ['ngResource'])
+.factory('actionsService', function($resource,$http,growl) {
+  var actionsService = {};
+
+  var Actions =  $resource('/actions/', {},{
+   byType:{method : "GET", params:{type:'@type'},url:'/actions/type/:type'}
+  });
+
+  actionsService.loadActions = function(type) {
+	return Actions.byType({type:type}).$promise.then(function(actions) {
+         return  _.chain(actions).values().pluck('actions').first().keys().value(); 
+	});
+  };
+ 
+  actionsService.launchAction = function(id,action){
+    $http.post('/jobs/'+action+'/'+id)
+      .success(function(data) {
+        growl.addInfoMessage(data.msg);
+      }).error(function(data){
+        growl.addErrorMessage(data.msg);
+    });
+  };
+
+  return actionsService;
+});
