7b66ed039255e5f2dfa30f86c269db7726349883
Ronen Narkis <narkisr@gmail.com>
2013-10-16 19:58:47 +0300

    moving to chrome

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -53,7 +53,7 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'Firefox'
+      'Chrome'
     ]
   });
 };
