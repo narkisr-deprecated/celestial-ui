4a20c5b26ea48350b0934f2697d9f5e14b415e1c
narkisr <narkisr@gmail.com>
2014-03-03 18:20:16 +0200

    listing user names and hypervisors

--- src/app/admin/quotas/add/add.js
+++ src/app/admin/quotas/add/add.js
@@ -11,7 +11,7 @@ angular.module( 'celestial.quotaAdd', [ ])
     data:{ pageTitle: 'New Quota' }
   });
 })
-.controller( 'QuotaAddCtrl', function UserAddController($scope, $resource, $location, growl, envsService) {
+.controller( 'QuotaAddCtrl', function UserAddController($scope, $resource, $location, growl, envsService, usersService) {
 
   var Users = $resource('/quota/',{});
   
@@ -19,6 +19,21 @@ angular.module( 'celestial.quotaAdd', [ ])
 
   $scope.submit = function(){
   };
-  
+
+  usersService.grabUsers().then(function(users){
+    $scope.users = users;
+    $scope.usernames = _.map(users, function(user){return user.username;});
+    $scope.user = $scope.users[0];
+  });
+ 
+  $scope.setHypervisors = function() {
+    if($scope.env !== undefined){
+      $scope.hypervisors = _.keys($scope.rawEnvs[$scope.env]); 
+      $scope.currentHypervisor = $scope.hypervisors[0];
+    }
+  };
+
+  $scope.$watch('env', $scope.setHypervisors);
+  // $scope.$watch('user', $scope.setHypervisors);
   envsService.loadEnvs($scope);
 });
