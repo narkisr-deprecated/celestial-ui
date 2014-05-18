cbaa0eff6ef452dde0e842b653847cd5d629fe34
narkisr <narkisr@gmail.com>
2014-05-19 02:25:00 +0300

    done and running jobs resources'

--- src/app/jobs/jobs.js
+++ src/app/jobs/jobs.js
@@ -17,21 +17,24 @@ angular.module( 'celestial.jobs', [
 
 .controller( 'JobsCtrl', function JobsCtrl( $scope, $resource,$timeout) {
   var Jobs = $resource('/jobs/',{},{
-    fetch:{method : "GET", isArray:false}
+    done:{method : "GET",url:'/jobs/done', isArray:false},
+    running:{method : "GET", params:{page:'@page', offset:'@offset'}, url:'/jobs/running', isArray:false}
   });
 
   $scope.jobs = [];
 
   (function tick() {
-    Jobs.fetch({},function(data,resp){
-       $scope.jobs = data.jobs;
-       var byStart = _.sortBy(data.erroneous.concat(data.succesful), function(s) {return s.end;}).reverse();
-       $scope.statuses = _.map(byStart, function(s) {
-         s.start = moment(s.start).format('MMMM Do YYYY, h:mm:ss a');
-         s.end = moment(s.end).format('MMMM Do YYYY, h:mm:ss a');
-         return s;
-       });
-       $timeout(tick, 5000);
-    });
+    Jobs.running({},function(data,resp){
+        $scope.jobs = data.jobs;
+        Jobs.done({page:1, offset:10},function(data,resp){
+         var byStart = _.sortBy(data.jobs, function(s) {return s.end;}).reverse();
+         $scope.statuses = _.map(byStart, function(s) {
+           s.start = moment(s.start).format('MMMM Do YYYY, h:mm:ss a');
+           s.end = moment(s.end).format('MMMM Do YYYY, h:mm:ss a');
+          return s;
+        });
+        $timeout(tick, 5000);
+      });
+   });
   })();
 });
