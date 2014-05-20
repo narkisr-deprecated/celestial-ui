e03c470c86339cc0901e9e7651d7cc3876be7bc4
narkisr <narkisr@gmail.com>
2014-05-20 03:00:16 +0300

    done job listing done

--- src/app/jobs/jobs.js
+++ src/app/jobs/jobs.js
@@ -26,15 +26,18 @@ angular.module( 'celestial.jobs', [
   (function tick() {
     Jobs.running({},function(data,resp){
         $scope.jobs = data.jobs;
-        Jobs.done({page:1, offset:10},function(data,resp){
-         var byStart = _.sortBy(data.jobs, function(s) {return s.end;}).reverse();
-         $scope.statuses = _.map(byStart, function(s) {
-           s.start = moment(s.start).format('MMMM Do YYYY, h:mm:ss a');
-           s.end = moment(s.end).format('MMMM Do YYYY, h:mm:ss a');
-          return s;
-        });
         $timeout(tick, 5000);
-      });
-   });
+    });
+
+    Jobs.done({page:1, offset:10},function(data,resp){
+       var byStart = _.sortBy(data.jobs, function(s) {return s.end;}).reverse();
+        $scope.statuses = _.map(byStart, function(s) {
+        console.log(s);
+        var source = s['_source'];
+        source.start = moment(source.start).format('MMMM Do YYYY, h:mm:ss a');
+        source.end = moment(source.end).format('MMMM Do YYYY, h:mm:ss a');
+        return source;
+       });
+    });
   })();
 });
