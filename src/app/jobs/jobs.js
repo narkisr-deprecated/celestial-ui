b92aab61fa1a09806a354d666a33ce3d4c96683a
narkisr <narkisr@gmail.com>
2014-05-27 20:03:55 +0300

    using source directly

--- src/app/jobs/jobs.js
+++ src/app/jobs/jobs.js
@@ -31,9 +31,7 @@ angular.module( 'celestial.jobs', [
 
     Jobs.done({page:1, offset:10},function(data,resp){
        var byStart = _.sortBy(data.jobs, function(s) {return s.end;}).reverse();
-        $scope.statuses = _.map(byStart, function(s) {
-        console.log(s);
-        var source = s['_source'];
+        $scope.statuses = _.map(byStart, function(source) {
         source.start = moment(source.start).format('MMMM Do YYYY, h:mm:ss a');
         source.end = moment(source.end).format('MMMM Do YYYY, h:mm:ss a');
         return source;
